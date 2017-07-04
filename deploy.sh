86cc7da15c79d380e0d180e52c4dabd2e8964498
Robert Lee-Cann <leeky@leeky.org.uk>
2017-07-04 14:43:56 +0100

    Tweak script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-set -ev && \
-cd ./dist && \
-remote_repo="https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" && \
-remote_branch="gh-pages" && \
-git init && \
-git config user.name "Travis CI" && \
-git config user.email "nobody@nobody.org" && \
-git add . && \
-git commit -m'build' && \
-git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
-rm -fr .git && \
+set -ev
+bundle exec ruby build.rb
+
+remote_repo=`git remote get-url origin`
+remote_branch="gh-pages"
+
+cd ./dist
+git init
+git add .
+git commit -m "build - `date`"
+git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1
+rm -fr .git
 cd ../
